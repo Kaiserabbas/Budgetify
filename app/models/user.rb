1be4f3c5b935ab55d226bbadd2bfec3e491f4eac
Qaisar <kayser.abbas@gmail.com>
2023-12-04 14:46:58 +0500

    Add method role == admin and skip confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :entities
   has_many :groups
+  attr_accessor :confirmation_sent_at
+  after_create :skip_confirmation!
 
   validates :name, presence: true
   validates :email, presence: true
+
+  def skip_confirmation!
+    confirm
+  end
+
+  def admin?
+    role == 'admin'
+  end 
 end
