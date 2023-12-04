339063b31d5dd4a599ead842e59b7a8f72694866
Qaisar <kayser.abbas@gmail.com>
2023-12-04 10:04:10 +0500

    Add group model

---
+++ app/models/group.rb
@@ -0,0 +1,11 @@
+class Group < ApplicationRecord
+  belongs_to :user
+  has_many :entities, dependent: :destroy  
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def total
+    entities.sum(:amount)
+  end
+end
