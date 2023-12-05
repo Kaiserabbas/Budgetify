51e404f6b6108fd3328108d1a7e895429a2cf547
Qaisar <kayser.abbas@gmail.com>
2023-12-05 15:59:57 +0500

    Run db:migrate for schema updation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_091518) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_061458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_091518) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
     t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
@@ -44,11 +46,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_091518) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "groups"
   add_foreign_key "entities", "users"
   add_foreign_key "groups", "users"
 end
