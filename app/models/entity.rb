e5641da93148ead301d72235b638b50f470c1866
Qaisar <kayser.abbas@gmail.com>
2023-12-05 16:04:23 +0500

    Add entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,8 +1,8 @@
 class Entity < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :groups
+  belongs_to :group
+  has_many :groups
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :icon, presence: true
 end
