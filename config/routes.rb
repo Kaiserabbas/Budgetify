5c9507fbb7da50c727c715019136d173abd965be
Qaisar <kayser.abbas@gmail.com>
2023-12-05 16:00:16 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,20 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+  devise_scope :user do
+    authenticated :user do
+      root :to => "groups#index", as: :authenticated_root
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+    unauthenticated :user do
+      root :to => "welcome#home", as: :unauthenticated_root
+    end
+  end
+
+  resources :groups, only: [:index, :new, :create, :show] do
+    resources :entities, only: [:index, :new, :create, :destroy, :edit, :update]
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
