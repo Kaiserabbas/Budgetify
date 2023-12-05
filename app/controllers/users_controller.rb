a03f41c778953a7b70ea716f1e61b165cdc7017c
Qaisar <kayser.abbas@gmail.com>
2023-12-05 16:05:58 +0500

    Delete user controller

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  before_action :authenticate_user! 
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @users = User.all
-  end
-end
