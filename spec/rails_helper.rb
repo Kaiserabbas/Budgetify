13fd5a45219e91e3cfed5dd6beb9495be3b40589
Qaisar <kayser.abbas@gmail.com>
2023-12-07 20:11:02 +0500

    Fix Rubocop linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'webdrivers'
@@ -14,19 +14,18 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
-  RSpec.configure do |config|
+RSpec.configure do |config|
   config.fixture_path = "#{Rails.root}/spec/fixtures"
   config.include Devise::Test::ControllerHelpers, type: :view
 
   config.use_transactional_fixtures = true
 
-   config.infer_spec_type_from_file_location!
+  config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
   config.before(:each, type: :system) do
     driven_by :rack_test
     Rails.application.reload_routes!
-
   end
 end
 Capybara.default_driver = :selenium_chrome
