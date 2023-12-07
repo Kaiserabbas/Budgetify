d1c9bd5deaf1d59d3b962ad10f6d0d5e3d9ec4b8
Qaisar <kayser.abbas@gmail.com>
2023-12-07 20:06:46 +0500

    Add accessor attribute :name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :entities
   has_many :groups
-  attr_accessor :confirmation_sent_at
+  attr_accessor :confirmation_sent_at, :name
 
   after_create :skip_confirmation!
 
